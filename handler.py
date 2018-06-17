974dd4aae9b696ce88013fe4cfcac60eefe73ebd
hiroyuki.nishi <hiroyuki.nishi@motex.co.jp>
2018-06-17 16:09:25 +0900

    リファクタリング

--- handler.py
+++ handler.py
@@ -4,27 +4,37 @@ import requests
 import urllib
 import os
 
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-message_header  = "============ Alart ===========\n"
+message_header = "============ Alart ===========\n"
+
 
 def hello(event, context):
     logger.info("Event: " + str(event))
-    CHATWORK_API_KEY = os.environ['CHATWORK_API_KEY']
-    CHATWORK_HEADER = os.environ['CHATWORK_HEADER']
-    CHATWORK_ROOM_ID = os.environ['CHATWORK_ROOM_ID']
-    CHATWORK_URL = os.environ['CHATWORK_URL']
-    URL = '{0}/rooms/{1}/messages'.format(CHATWORK_URL,CHATWORK_ROOM_ID)
-    chatwork_message = "hoge"
-    payload = {'body': chatwork_message}
-    headers = {CHATWORK_HEADER: CHATWORK_API_KEY}
+    __company_info(event)
+
+
+def __company_info(event):
+    __url = '{0}/rooms/{1}/messages'.format(os.environ['CHATWORK_URL'], os.environ['CHATWORK_ROOM_ID'])
+    __payload = {'body': __parse(event)}
+    __post(__url, __payload)
+
+
+def __post(url, payload):
     try:
-        requests.post(URL, headers=headers, params=payload)
+        requests.post(url, headers=__header(), params=payload)
     except:
-        logger.info("Failed! Message : \n" + str(chatwork_message))
+        logger.info("Failed! Message : \n" + str(__message))
     else:
-        logger.info("Success! Message posted to: \n" + str(chatwork_message))
+        logger.info("Success! Message posted to: \n" + str(__message))
 
 
+def __header():
+    return {os.environ['CHATWORK_HEADER']: os.environ['CHATWORK_API_KEY']}
+
+
+def __parse(event):
+    __body = json.loads(event['body'])
+    logger.info(__body['webhook_event']['body'])
+    return __body['webhook_event']['body']
