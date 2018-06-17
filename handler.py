e9afbefd5a157cff50aae21e02807ed925e12ed2
hiroyuki.nishi <hiroyuki.nishi@motex.co.jp>
2018-06-17 13:17:57 +0900

    first commit

---
+++ handler.py
@@ -0,0 +1,32 @@
+import json
+import logging
+import requests
+import urllib
+import os
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+message_header  = "============ Alart ===========\n"
+
+def hello(event, context):
+    logger.info("Event: " + str(event))
+    message = json.loads(event['Records'][0]['Sns']['Message'])
+    logger.info("Message: " + str(message))
+    CHATWORK_API_KEY = os.environ['CHATWORK_API_KEY']
+    CHATWORK_HEADER = os.environ['CHATWORK_HEADER']
+    CHATWORK_ROOM_ID = os.environ['CHATWORK_ROOM_ID']
+    CHATWORK_URL = os.environ['CHATWORK_URL']
+    URL = '{0}/rooms/{1}/messages'.format(CHATWORK_URL,CHATWORK_ROOM_ID)
+    chatwork_message = "hoge"
+    payload = {'body': chatwork_message}
+    headers = {CHATWORK_HEADER: CHATWORK_API_KEY}
+    try:
+        requests.post(URL, headers=headers, params=payload)
+    except:
+        logger.info("Failed! Message : \n" + str(chatwork_message))
+    else:
+        logger.info("Success! Message posted to: \n" + str(chatwork_message))
+
+
